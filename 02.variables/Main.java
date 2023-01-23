eaa3b084f476036dc57747ea95c5108b5dd0ee45
karan51ngh <karansingh9535@gmail.com>
2023-01-23 14:07:24 +0530

    update

--- 02.variables/Main.java
+++ 02.variables/Main.java
@@ -9,6 +9,17 @@ public class Main {
 
         int summ = n1+n2+n3;
         System.out.println(summ);
+
+        
+        byte age = 64;
+        long agedino = 12344344535L;
+        char ch = 'b';
+        float f1 = 5.6f;
+        double d1 =5.6;
+
+        String str = "karan51ngh";
+        // types are classes in Java
+
     }
     
 }
