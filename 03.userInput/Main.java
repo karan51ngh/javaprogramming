81c48823a3fba28930e536488d2b18afbfcaa957
karan51ngh <karansingh9535@gmail.com>
2023-01-23 14:30:00 +0530

    user input

---
+++ 03.userInput/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("input from keyboard");
+
+        Scanner sc = new Scanner(System.in);
+
+        // System.out.println("Enter n1");
+        // int n1 = sc.nextInt();
+        // System.out.println("Enter n2");
+        // int n2 = sc.nextInt();
+
+        // int summ = n1+n2;
+        // System.out.println("Sum of the numbers is " + summ);
+
+        // String str = sc.next(); // untill white space
+        // System.out.println(str);
+
+        String str2 = sc.nextLine();
+        System.out.println(str2);
+
+    }
+    
+}
