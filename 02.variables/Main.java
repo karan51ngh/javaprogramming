305f8448d1409b839333bcfff80ffc2a5dbaf83c
karan51ngh <karansingh9535@gmail.com>
2023-01-23 13:51:26 +0530

    var intro

---
+++ 02.variables/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("The sum is: ");
+
+        int n1 = 1;
+        int n2 = 2;
+        int n3 = 3;
+
+        int summ = n1+n2+n3;
+        System.out.println(summ);
+    }
+    
+}
