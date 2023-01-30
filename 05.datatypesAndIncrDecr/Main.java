65180b3bbd56e3026ed69340de7f357fca7cf772
karan51ngh <karansingh9535@gmail.com>
2023-01-30 11:49:31 +0530

    datatypes and increment

---
+++ 05.datatypesAndIncrDecr/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        /* 
+        byte x = 5;
+        int y = 6;
+        short z = 8;
+        int a = y + z;
+        float b = 6.54f + x;
+        System.out.println(b); 
+        */
+
+        // Increment and Decrement Operators
+        int i = 56;
+        int j = 67;
+        int c = ++j; // first j is incremented then c is assigned j (68)
+        System.out.println(c);
+
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+        
+        int y = 7;
+        System.out.println( ++y *8);
+        char ch = 'a';
+        System.out.println(++ch);
+    }
+}
