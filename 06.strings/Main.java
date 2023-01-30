2f765babb7b90451c4977f87bfca2392b94eddd4
karan51ngh <karansingh9535@gmail.com>
2023-01-30 12:15:26 +0530

    strings def/inp

--- 06.strings/Main.java
+++ 06.strings/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // String is a Class in java
@@ -10,5 +12,11 @@ public class Main {
         float b = 5.6454f;
         System.out.printf("The value of a is %d and value of b is %f\n", a, b);
         //System.out.format("The value of a is %d and value of b is %f", a, b);
+
+        // user input in string
+        Scanner sc = new Scanner(System.in);
+        String st = sc.next();
+        System.out.println(st);
+        sc.close();
     }   
 }
