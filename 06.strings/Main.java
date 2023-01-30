f042f716929f1c182a8af1c52604e2782a28c107
karan51ngh <karansingh9535@gmail.com>
2023-01-30 12:12:13 +0530

    strings

---
+++ 06.strings/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // String is a Class in java
+        String name = new String("karan51ngh");
+        // String name = "karan51ngh";
+        System.out.print("The name is: ");
+        System.out.print(name);
+
+        int a = 6;
+        float b = 5.6454f;
+        System.out.printf("The value of a is %d and value of b is %f\n", a, b);
+        //System.out.format("The value of a is %d and value of b is %f", a, b);
+    }   
+}
