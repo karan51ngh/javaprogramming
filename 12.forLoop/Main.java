e28ec9f5bbb4002a3bbc98119b0fc59b311cb8af
karan51ngh <karansingh9535@gmail.com>
2023-01-31 12:01:39 +0530

    for loop

---
+++ 12.forLoop/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+       for (int i=1; i<=10; i++){
+           System.out.println(i);
+       }
+
+        for(int i=5; i!=0; i--){
+            System.out.println(i);
+        }
+    }
+}
