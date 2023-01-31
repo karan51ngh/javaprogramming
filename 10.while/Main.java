63f497470fa2038ffb2942ede8b94674291f32c3
karan51ngh <karansingh9535@gmail.com>
2023-01-31 11:55:01 +0530

    while loop

---
+++ 10.while/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 190;
+        while(i<=200){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Finished Running While Loop!");
+
+//        while(true){
+//            System.out.println("infinite loop");
+//        }
+    }
+}
