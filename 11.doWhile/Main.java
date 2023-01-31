2c36909d2f01ba2f5ddee11d3277ec220b0653ea
karan51ngh <karansingh9535@gmail.com>
2023-01-31 11:58:13 +0530

    do while loop

---
+++ 11.doWhile/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        do {
+            System.out.println(a);
+            a++;
+        }while(a<5);
+    }
+}
